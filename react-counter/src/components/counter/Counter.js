ce39aae4d16f18f7cf05ee6bea973d782f424e82
milomacphail <milomacphail@gmail.com>
2020-06-07 15:03:01 -0400

    Counter preserves state

--- react-counter/src/components/counter/Counter.js
+++ react-counter/src/components/counter/Counter.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Counter.css';
 
-function Counter() {
-  return (
-    <div className="counter">
-      <button onClick={increment}>+1</button>
-      <span className="count">0</span>
-    </div>
-  );
-}
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
 
-function increment() {
-  console.log('increment');
+    this.increment = this.increment.bind(this);
+  }
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={this.increment}>+1</button>
+        <span className="count">{this.state.counter}</span>
+      </div>
+    );
+  }
+
+  increment() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
 }
 
 export default Counter;
